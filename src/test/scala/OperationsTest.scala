e610793eef825bffb99b2d96d239dd663a11236d
Tony Tam <fehguy@gmail.com>
2014-07-09 23:06:21 -0700

    added resource and operation schemas

---
+++ src/test/scala/OperationsTest.scala
@@ -0,0 +1,41 @@
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.fge.jsonschema.core.exceptions.ProcessingException
+import com.github.fge.jsonschema.main.{ JsonSchema, JsonSchemaFactory}
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jackson.JsonLoader
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+import scala.io.Source
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class OperationsTest extends FlatSpec with ShouldMatchers {
+  val mapper = new ObjectMapper
+  val schema = mapper.readTree(Source.fromFile("schemas/v2.0/operationSchema.json").mkString)
+  val factory = JsonSchemaFactory.byDefault()
+  val jsonSchema = factory.getJsonSchema(schema)
+
+  it should "validate a resource with string path param" in {
+    val json = Source.fromFile("samples/v2.0/json/resources/operations/stringPathParamResource.json").mkString
+    val data = JsonLoader.fromString(json)
+    val report = jsonSchema.validate(data)
+    if(report.isSuccess == false)
+      println(report)
+    report.isSuccess should be (true)
+  }
+
+  it should "validate a resource with string path param and boolean query param" in {
+    val json = Source.fromFile("samples/v2.0/json/resources/operations/stringPathAndBoolQueryParamResource.json").mkString
+    val data = JsonLoader.fromString(json)
+    val report = jsonSchema.validate(data)
+    if(report.isSuccess == false)
+      println(report)
+    report.isSuccess should be (true)
+  }
+
+}

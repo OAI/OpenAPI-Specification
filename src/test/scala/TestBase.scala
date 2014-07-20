9cd3500baa2aaff644c0c9afed146b4c97770956
Tony Tam <fehguy@gmail.com>
2014-07-20 16:37:43 -0600

    added url rewrite to make rests pass with java validator

--- src/test/scala/TestBase.scala
+++ src/test/scala/TestBase.scala
@@ -3,12 +3,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import scala.io.Source
 
 /**
- * makes the schemas absolute so the json-schema validator can locate
- * references
+ * makes the schemas absolute so the java json-schema validator can locate
+ * references correctly
  **/
 trait TestBase {
   val mapper = new ObjectMapper
-  val host = "http://localhost:8000/schema.json"
+  val host = "https://raw.githubusercontent.com/reverb/swagger-spec/master/schemas/v2.0/schema.json"
 
   def readSchema(makeQualified: Boolean = true) = {
     val json = Source.fromFile("schemas/v2.0/schema.json").getLines

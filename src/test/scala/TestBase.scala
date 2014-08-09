9a63749b94284446861209b16a30b62ccc8f3e47
Tony Tam <fehguy@gmail.com>
2014-08-09 12:03:14 -0700

    put test base back to github

--- src/test/scala/TestBase.scala
+++ src/test/scala/TestBase.scala
@@ -8,8 +8,8 @@ import scala.io.Source
  **/
 trait TestBase {
   val mapper = new ObjectMapper
-  // val host = "https://raw.githubusercontent.com/reverb/swagger-spec/master/schemas/v2.0/schema.json"
-  val host = "http://localhost:8000/schema.json"
+  val host = "https://raw.githubusercontent.com/reverb/swagger-spec/master/schemas/v2.0/schema.json"
+  // val host = "http://localhost:8000/schema.json"
 
   def readSchema(makeQualified: Boolean = true) = {
     val json = Source.fromFile("schemas/v2.0/schema.json").getLines

5247d295a08ee95781d69b750ea366e3410ce197
Tony Tam <fehguy@gmail.com>
2014-08-15 18:43:07 -0700

    updated tests to reflect new locations

--- src/test/scala/ResourcesTest.scala
+++ src/test/scala/ResourcesTest.scala
@@ -18,7 +18,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   val jsonSchema = factory.getJsonSchema(schema)
 
   it should "validate a resource with string path param" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/stringPathParamResource.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/stringPathParamResource.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -27,7 +27,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a spec with common params" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/commonParameters.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/commonParameters.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -36,7 +36,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a spec with vendor extensions" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/vendorExtensionExamples.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/vendorExtensionExamples.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -45,7 +45,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a resource with example payload" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/resourceWithExamplePayload.json").getLines.filter(!_.startsWith("//")).mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/resourceWithExamplePayload.json").getLines.filter(!_.startsWith("//")).mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -54,7 +54,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a resource with cascading schemes" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/cascadingSchemes.json").getLines.filter(!_.startsWith("//")).mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/cascadingSchemes.json").getLines.filter(!_.startsWith("//")).mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -63,7 +63,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a spec with relative host" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/resourceWithRelativeHost.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/resourceWithRelativeHost.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -72,7 +72,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a spec with linked resources" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/resourceWithLinkedDefinitions.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/resourceWithLinkedDefinitions.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -81,7 +81,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate a spec with multiple mime types" in {
-    val json = Source.fromFile("samples/v2.0/json/resources/multipleMimeTypes.json").mkString
+    val json = Source.fromFile("fixtures/v2.0/json/resources/multipleMimeTypes.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -90,7 +90,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate the wordnik petstore" in {
-    val json = Source.fromFile("examples/wordnik/petstore.json").mkString
+    val json = Source.fromFile("examples/v2.0/json/petstore.json").mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -99,7 +99,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate mads sample 1" in {
-    val json = Source.fromFile("examples/mads/petstore-simple.json").getLines.filter(!_.startsWith("//")).mkString
+    val json = Source.fromFile("examples/v2.0/json/petstore-simple.json").getLines.filter(!_.startsWith("//")).mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)
@@ -108,7 +108,7 @@ class ResourcesTest extends FlatSpec with ShouldMatchers with TestBase {
   }
 
   it should "validate mads sample 2" in {
-    val json = Source.fromFile("examples/mads/petstore-expanded.json").getLines.filter(!_.startsWith("//")).mkString
+    val json = Source.fromFile("examples/v2.0/json/petstore-expanded.json").getLines.filter(!_.startsWith("//")).mkString
     val data = JsonLoader.fromString(json)
     val report = jsonSchema.validate(data)
     if(report.isSuccess == false)

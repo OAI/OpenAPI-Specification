dd9d2790582e974e2f4ed11fae757caa97360ad2
Tony Tam <fehguy@gmail.com>
2014-08-26 01:31:35 -0700

    reverted test path

--- src/test/scala/TestBase.scala
+++ src/test/scala/TestBase.scala
@@ -8,7 +8,7 @@ import scala.io.Source
  **/
 trait TestBase {
   val mapper = new ObjectMapper
-  val host = "https://raw.githubusercontent.com/fehguy/swagger-spec/master/schemas/v2.0/schema.json"
+  val host = "https://raw.githubusercontent.com/reverb/swagger-spec/master/schemas/v2.0/schema.json"
   // val host = "http://localhost:8000/schema.json"
 
   def readSchema(makeQualified: Boolean = true) = {

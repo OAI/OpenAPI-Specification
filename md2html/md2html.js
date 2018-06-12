615c4e8dc7dcbc54d4eed26f27ada2b4f45ac93f
Mike Ralphson <mike.ralphson@gmail.com>
2018-06-13 00:41:42 +0100

    Github gist syntax highlighting css

--- md2html/md2html.js
+++ md2html/md2html.js
@@ -81,6 +81,7 @@ function preface(title,options) {
         preface += 'table tr { background-color: #fff; border-top: 1px solid #c6cbd1; }';
         preface += 'table tr:nth-child(2n) { background-color: #f6f8fa; }';
         preface += 'pre { background-color: #f6f8fa !important; }';
+        preface += fs.readFileSync('./gist.css','utf8').split('\n').join(' ');
         preface += '</style>';
         preface += '<section id="abstract">';
         preface += 'The OpenAPI Specification (OAS) defines a standard, programming language-agnostic interface description for REST APIs, which allows both humans and computers to discover and understand the capabilities of a service without requiring access to source code, additional documentation, or inspection of network traffic. When properly defined via OpenAPI, a consumer can understand and interact with the remote service with a minimal amount of implementation logic. Similar to what interface descriptions have done for lower-level programming, the OpenAPI Specification removes guesswork in calling a service.';
@@ -283,7 +284,7 @@ for (let l in lines) {
 
 }
 
-fs.writeFileSync('./md2html.tmp',lines.join('\n'),'utf8');
+//fs.writeFileSync('./md2html.tmp',lines.join('\n'),'utf8');
 
 s = preface('OpenAPI Specification',argv)+'\n\n'+lines.join('\n');
 let out = md.render(s);

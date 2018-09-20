678ca708f72413573656b0dcd919c817cf288fc1
Mike Ralphson <mike.ralphson@gmail.com>
2018-09-20 08:18:01 +0100

    md2html; build putative versions 4,5 etc

--- scripts/md2html/build.sh
+++ scripts/md2html/build.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
+mkdir -p deploy/oas
+mkdir -p deploy/js
+cd scripts/md2html
+cp -p js/* ../../deploy/js
+cp -p markdown/* ../../deploy/
 
-#node md2html.js ../versions/3.0.0.md > v3/3.0.0.bs
-#cat v3/h3.0.0.bs style-start.html syntax-github.css style-finish.html v3/3.0.0.bs > v3/input.bs
-#curl https://api.csswg.org/bikeshed/ -F file=@v3/input.bs -F output=err > v3/oas_3.0.0.err
-#curl https://api.csswg.org/bikeshed/ -F file=@v3/input.bs -F force=1 > v3/oas_3.0.0.html
+node md2html.js --respec --maintainers ./history/MAINTAINERS_v2.0.md ../../versions/2.0.md > ../../deploy/oas/v2.0.html
 
-node md2html.js ../versions/3.0.1.md > v3/3.0.1.bs
-cat v3/h3.0.1.bs style-start.html syntax-github.css style-finish.html v3/3.0.1.bs > v3/input.bs
-curl https://api.csswg.org/bikeshed/ -F file=@v3/input.bs -F output=err > v3/oas_3.0.1.err
-curl https://api.csswg.org/bikeshed/ -F file=@v3/input.bs -F force=1 > v3/oas_3.0.1.html
+for filename in ../../versions/[3456789].*.md ; do
+  node md2html.js --respec --maintainers ../../MAINTAINERS.md ${filename} > ../../deploy/oas/v$(basename "$filename" .md).html
+done
 
-#node md2html.js ../versions/2.0.md > v2/2.0.bs
-#cat v2/heading.bs style-start.html syntax-github.css style-finish.html v2/2.0.bs > v2/input.bs
-#curl https://api.csswg.org/bikeshed/ -F file=@v2/input.bs -F output=err > v2/oas_2.0.err
-#curl https://api.csswg.org/bikeshed/ -F file=@v2/input.bs -F force=1 > v2/oas_2.0.html
--- scripts/md2html/respec.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-node md2html.js --maintainers ../../MAINTAINERS.md ../../versions/3.0.1.md > respec.html

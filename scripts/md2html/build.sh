e7a59992cfd4f34cf4d22eb322102622681bfb0a
Bellangelo <aggelosbellos7@gmail.com>
2024-06-05 01:28:44 +0300

    Build site without the deploy folder

--- scripts/md2html/build.sh
+++ scripts/md2html/build.sh
@@ -5,9 +5,6 @@
 # run this script from the root of the repo. It is designed to be run by a GitHub workflow.
 # It contains bashisms
 
-mkdir -p deploy/oas
-mkdir -p deploy/js
-
 cd scripts/md2html
 mkdir -p history
 cat > history/MAINTAINERS_v2.0.md <<EOF
@@ -33,18 +30,18 @@ cp history/MAINTAINERS_v3.0.2.md history/MAINTAINERS_v3.0.3.md
 cp history/MAINTAINERS_v3.0.2.md history/MAINTAINERS_v3.1.0.md
 # add lines for 3.0.4, 3.1.1, ...
 
-cp -p js/* ../../deploy/js 2> /dev/null
-cp -p markdown/* ../../deploy/ 2> /dev/null
+cp -p js/* ../../docs/js 2> /dev/null
+cp -p markdown/* ../../docs/ 2> /dev/null
 
 latest=`git describe --abbrev=0 --tags`
 latestCopied=none
 for filename in ../../versions/[23456789].*.md ; do
   version=$(basename "$filename" .md)
-  node md2html.js --respec --maintainers ./history/MAINTAINERS_v$version.md ${filename} > ../../deploy/oas/v$version.html
+  node md2html.js --respec --maintainers ./history/MAINTAINERS_v$version.md ${filename} > ../../docs/oas/v$version.html
   if [ $version = $latest ]; then
     if [[ ${version} != *"rc"* ]];then
       # version is not a Release Candidate
-      cp -p ../../deploy/oas/v$version.html ../../deploy/oas/latest.html
+      cp -p ../../docs/oas/v$version.html ../../docs/oas/latest.html
       latestCopied=v$version
     fi
   fi

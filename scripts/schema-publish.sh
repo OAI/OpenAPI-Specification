98f055fd22106a5a9a0e7a23d6e71b0ec8ddaf23
Ralf Handl <ralf.handl@gmail.com>
2024-10-16 21:21:21 +0200

    Update schema-publish.sh

--- scripts/schema-publish.sh
+++ scripts/schema-publish.sh
@@ -5,21 +5,20 @@
 # run this script from the root of the repo. It is designed to be run by a GitHub workflow.
 
 for filename in schemas/v3*/schema.yaml ; do
-  version=$(basename $(dirname "$filename"))
+  vVersion=$(basename $(dirname "$filename"))
+  version=${vVersion:1}
   lastCommitDate=$(git log -1 --format="%ad" --date=format:"%Y%m%d" "$filename")
 
   echo "$filename $lastCommitDate"
   mkdir -p deploy/oas/$version/schema
   node scripts/schema-convert.js "$filename" $lastCommitDate > deploy/oas/$version/schema/$lastCommitDate
-  # mv deploy/oas/$version/schema/*.md deploy/oas/$version/schema/$lastCommitDate.md
-  ls deploy/oas/$version/schema/*
+  mv deploy/oas/$version/schema/*.md deploy/oas/$version/schema/$lastCommitDate.md
 
   filenameBase=$(dirname "$filename")/schema-base.yaml
   if [ -f "$filenameBase" ]; then
     echo "$filenameBase $lastCommitDate"
     mkdir -p deploy/oas/$version/schema-base
     node scripts/schema-convert.js "$filenameBase" $lastCommitDate > deploy/oas/$version/schema-base/$lastCommitDate
-    # mv deploy/oas/$version/schema-base/*.md deploy/oas/$version/schema-base/$lastCommitDate.md
-    ls deploy/oas/$version/schema-base/*
+    mv deploy/oas/$version/schema-base/*.md deploy/oas/$version/schema-base/$lastCommitDate.md
   fi
 done

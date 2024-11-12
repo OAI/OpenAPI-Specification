1f2a389b619bf9425bd92b547355e82ab91dc0b5
Ralf Handl <ralf.handl@sap.com>
2024-11-12 12:51:33 +0100

    Prettier

--- scripts/schema-test-coverage.mjs
+++ scripts/schema-test-coverage.mjs
@@ -4,7 +4,13 @@ import { join } from "node:path";
 import { argv } from "node:process";
 import "@hyperjump/json-schema/draft-2020-12";
 import "@hyperjump/json-schema/draft-04";
-import { compile, getSchema, interpret, Validation, BASIC } from "@hyperjump/json-schema/experimental";
+import {
+  compile,
+  getSchema,
+  interpret,
+  Validation,
+  BASIC,
+} from "@hyperjump/json-schema/experimental";
 import * as Instance from "@hyperjump/json-schema/instance/experimental";
 
 /**
@@ -18,18 +24,24 @@ import { buildSchemaDocument } from "@hyperjump/json-schema/experimental";
 
 addMediaTypePlugin("application/schema+yaml", {
   parse: async (response) => {
-    const contentType = contentTypeParser.parse(response.headers.get("content-type") ?? "");
-    const contextDialectId = contentType.parameters.schema ?? contentType.parameters.profile;
+    const contentType = contentTypeParser.parse(
+      response.headers.get("content-type") ?? "",
+    );
+    const contextDialectId =
+      contentType.parameters.schema ?? contentType.parameters.profile;
 
     const foo = YAML.parse(await response.text());
     return buildSchemaDocument(foo, response.url, contextDialectId);
   },
-  fileMatcher: (path) => path.endsWith(".yaml")
+  fileMatcher: (path) => path.endsWith(".yaml"),
 });
 
 /** @type (testDirectory: string) => AsyncGenerator<[string,Json]> */
 const tests = async function* (testDirectory) {
-  for (const file of await readdir(testDirectory, { recursive: true, withFileTypes: true })) {
+  for (const file of await readdir(testDirectory, {
+    recursive: true,
+    withFileTypes: true,
+  })) {
     if (!file.isFile() || !file.name.endsWith(".yaml")) {
       continue;
     }
@@ -100,12 +112,21 @@ Validation.interpret = baseInterpret;
 // console.log("Covered:", visitedLocations);
 
 const allKeywords = keywordLocations(compiled.ast);
-const notCovered = allKeywords.filter((location) => !visitedLocations.has(location));
-console.log("NOT Covered:", notCovered.length, "of", allKeywords.length,);
+const notCovered = allKeywords.filter(
+  (location) => !visitedLocations.has(location),
+);
+if (notCovered.length > 0) {
+  console.log("NOT Covered:", notCovered.length, "of", allKeywords.length);
+  const maxNotCovered = 20;
+  const firstNotCovered = notCovered.slice(0, maxNotCovered);
+  if (notCovered.length > maxNotCovered) firstNotCovered.push("...");
+  console.log(firstNotCovered);
+}
 
-const maxNotCovered = 20;
-const firstNotCovered = notCovered.slice(0, maxNotCovered);
-if (notCovered.length > maxNotCovered) firstNotCovered.push("...");  
-console.log(firstNotCovered);
-
-console.log("Covered:", visitedLocations.size, "of", allKeywords.length, "(" + Math.floor(visitedLocations.size / allKeywords.length * 100) + "%)");
+console.log(
+  "Covered:",
+  visitedLocations.size,
+  "of",
+  allKeywords.length,
+  "(" + Math.floor((visitedLocations.size / allKeywords.length) * 100) + "%)",
+);

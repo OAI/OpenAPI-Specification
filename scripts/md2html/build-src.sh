a3425f2ffd67c3fdf289d85384110a63577a2fee
Ralf Handl <ralf.handl@gmail.com>
2024-12-19 20:55:56 +0100

    builld into deploy-preview

--- scripts/md2html/build-src.sh
+++ scripts/md2html/build-src.sh
@@ -6,21 +6,21 @@
 
 mkdir -p deploy/oas
 mkdir -p deploy/js
-mkdir -p src-preview
+mkdir -p deploy-preview
+
+cp -p node_modules/respec/builds/respec-w3c.* deploy/js/
 
 cd scripts/md2html
-
-cp -p ../../node_modules/respec/builds/respec-w3c.* ../../deploy/js/
 
 filename=src/oas.md
 tempfile=../../deploy/oas/oas.html
 echo "=== ${filename} ==="
 
 node md2html.js --maintainers ../../EDITORS.md ../../${filename} > $tempfile
-npx respec --use-local --src $tempfile --out ../../src-preview/oas.html
+npx respec --use-local --src $tempfile --out ../../deploy-preview/oas.html
 rm $tempfile
 
-echo Built src-preview/oas.html
+echo Built deploy-preview/oas.html
 echo
 
 rm ../../deploy/js/respec-w3c.*

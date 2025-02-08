9449ce841cbb32219bcaa68c4bde0080a1d8135e
Duncan Beevers <duncan@dweebd.com>
2025-02-08 05:04:27 -0800

    Build single targets
    build.sh with no arguments builds all versions
    build.sh <version> builds single version
    build.sh latest builds latest version
    build.sh src builds from src/oas.md
    Resolve Google Analytics snippet location relative to md2html.js
    Update tests to reflect inclusion of snippet
    Pre-requisite to https://github.com/OAI/OpenAPI-Specification/pull/4341

--- scripts/md2html/build.sh
+++ scripts/md2html/build.sh
@@ -2,46 +2,80 @@
 
 # Author: @MikeRalphson
 
-# run this script from the root of the repo. It is designed to be run by a GitHub workflow.
+# run this script from the root of the repo
+# It is designed to be run by a GitHub workflow
+
+# Usage: build.sh [version | "latest" | "src"]
+# When run with no arguments, it builds artifacts for all published specification versions.
+# It may also be run with a specific version argument, such as "3.1.1" or "latest"
+# Finally, it may be run with "src" to build "src/oas.md"
+#
 # It contains bashisms
 
-mkdir -p deploy/oas
-mkdir -p deploy/js
+if [ -z "$1" ]; then
+  deploydir="deploy/oas"
+else
+  deploydir="deploy-preview"
+fi
 
-cd scripts/md2html
+mkdir -p $deploydir/js
+mkdir -p $deploydir/temp
 
-cp -p ../../node_modules/respec/builds/respec-w3c.* ../../deploy/js/
-
-latest=`git describe --abbrev=0 --tags`
-latestCopied=none
+latest=$(git describe --abbrev=0 --tags)
+latestCopied="none"
 lastMinor="-"
-for filename in $(ls -1 ../../versions/[23456789].*.md | sort -r) ; do
-  if [[ ${filename} == *-editors.md ]];then
-    continue
+
+if [ -z "$1" ]; then
+  specifications=$(ls -1 versions/[23456789].*.md | grep -v -e "\-editors" | sort -r)
+elif [ "$1" = "latest" ]; then
+  specifications=$(ls -1 versions/$latest.md)
+elif [ "$1" = "src" ]; then
+  specifications="src/oas.md"
+else
+  specifications=$(ls -1 versions/$1.md)
+fi
+
+cp -p node_modules/respec/builds/respec-w3c.* $deploydir/js/
+
+for specification in $specifications; do
+  version=$(basename $specification .md)
+  minorVersion=${version:0:3}
+  tempfile="$deploydir/temp/$version.html"
+
+  if [ "$1" = "src" ]; then
+    destination="$deploydir/$version.html"
+    maintainers="EDITORS.md"
+  else
+    destination="$deploydir/v$version.html"
+    maintainers="$(dirname $specification)/$version-editors.md"
   fi
 
-  version=$(basename "$filename" .md)
-  minorVersion=${version:0:3}
-  tempfile=../../deploy/oas/v$version-tmp.html
-  echo -e "\n=== v$version ==="
+  echo === Building $version to $destination
 
-  node md2html.js --maintainers ../../versions/$version-editors.md ${filename} > $tempfile
-  npx respec --use-local --src $tempfile --out ../../deploy/oas/v$version.html
+  node scripts/md2html/md2html.js --maintainers $maintainers $specification > $tempfile
+  npx respec --use-local --src $tempfile --out $destination
   rm $tempfile
 
+  echo === Built $destination
+
   if [ $version = $latest ]; then
-    if [[ ${version} != *"rc"* ]];then
+    if [[ ${version} != *"rc"* ]]; then
       # version is not a Release Candidate
-      ( cd ../../deploy/oas && ln -sf v$version.html latest.html )
-      latestCopied=v$version
+      ln -sf $(basename $destination) $deploydir/latest.html
+      latestCopied="v$version"
     fi
   fi
 
   if [ ${minorVersion} != ${lastMinor} ] && [ ${minorVersion} != 2.0 ]; then
-    ( cd ../../deploy/oas && ln -sf v$version.html v$minorVersion.html )
+    ln -sf $(basename $destination) $deploydir/v$minorVersion.html
     lastMinor=$minorVersion
   fi
 done
-echo Latest tag is $latest, copied $latestCopied to latest.html
 
-rm ../../deploy/js/respec-w3c.*
+if [ "$latestCopied" != "none" ]; then
+  echo Latest tag is $latest, copied $latestCopied to latest.html
+fi
+
+rm $deploydir/js/respec-w3c.*
+rmdir $deploydir/js
+rmdir $deploydir/temp

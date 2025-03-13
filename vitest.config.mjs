cb37ec78d088430b51fb6610257b2534701c6133
Ralf Handl <ralf.handl@gmail.com>
2025-03-13 20:28:56 +0100

    Move old schemas to _archive_
    Update schema-test-coverage.sh

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     forceRerunTriggers: ['**/scripts/**', '**/tests/**'],
+    include: ['**/tests/**/*.test.mjs'],
   },
 })

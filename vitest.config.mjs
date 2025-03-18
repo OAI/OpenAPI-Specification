9c9506e8bed095619f8daed1895ef8da2643b581
Ralf Handl <ralf.handl@gmail.com>
2025-03-18 14:46:42 +0100

    reactivate v3.0 schema tests
    This tests whether dependabot breaks vitest

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     forceRerunTriggers: ['**/scripts/**', '**/tests/**'],
-    include: ['**/tests/**/*.test.mjs'],
   },
 })

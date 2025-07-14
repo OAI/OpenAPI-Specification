800542aab5fa0242f2b6dbf2fdf2874917b6e335
Jason Desrosiers <jdesrosi@gmail.com>
2025-07-13 22:18:28 -0700

    Thresholds now work with json-schema-coverage

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import { jsonSchemaCoveragePlugin } from "@hyperjump/json-schema-coverage/vitest"
 
 export default defineConfig({
+  plugins: [jsonSchemaCoveragePlugin()],
   test: {
     globalSetup: ["tests/schema/oas-schema.mjs"],
     coverage: {
-      provider: "custom",
-      customProviderModule: "@hyperjump/json-schema-coverage/vitest/coverage-provider",
-      include: ["src/schemas/validation/**/*.yaml"]
+      include: ["src/schemas/validation/**/*.yaml"],
+      thresholds: process.env.BASE !== "dev" ? {
+        100: true
+      } : {}
     },
     forceRerunTriggers: ['**/scripts/**', '**/tests/**'],
     testTimeout: 10000, // 10 seconds

5c0aa8bfc275ecec3bc9664404ae13a84d65791f
Ralf Handl <ralf.handl@gmail.com>
2025-07-15 00:17:29 +0200

    static coverage of schema.yaml

--- tests/schema/schema.test.mjs
+++ tests/schema/schema.test.mjs
@@ -13,7 +13,45 @@ await registerOasSchema();
 await registerSchema("./src/schemas/validation/schema.yaml");
 const fixtures = './tests/schema';
 
-describe("v3.1", () => {
+describe("v3.2", () => {
+  test("schema.yaml schema test", async () => {
+    // Hardcode this simple document instead of putting it in pass/fail directories because
+    // documents in those folders get run against schema-base.yaml instead of schema.yaml.
+    const oad = {
+      // Also need to include required properties
+      openapi: "3.2.0",
+      info: {
+        title: "API",
+        version: "1.0.0"
+      },
+      components: {
+        schemas: {
+          foo: {}
+        }
+      }
+    };
+    await expect(oad).to.matchJsonSchema("./src/schemas/validation/schema.yaml"); // <-- "schema.yaml" instead of "schema-base.yaml"
+  });
+
+  test("schema.yaml invalid Schema Object type", async () => {
+    // Hardcode this simple document instead of putting it in pass/fail directories because
+    // documents in those folders get run against schema-base.yaml instead of schema.yaml.
+    const oad = {
+      // Also need to include required properties
+      openapi: "3.2.0",
+      info: {
+        title: "API",
+        version: "1.0.0"
+      },
+      components: {
+        schemas: {
+          foo: 42
+        }
+      }
+    };
+    await expect(oad).to.not.matchJsonSchema("./src/schemas/validation/schema.yaml"); // <-- "schema.yaml" instead of "schema-base.yaml"
+  });
+
   describe("Pass", () => {
     readdirSync(`${fixtures}/pass`, { withFileTypes: true })
       .filter((entry) => entry.isFile() && /\.yaml$/.test(entry.name))

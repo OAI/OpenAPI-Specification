efe3b80099fa511c60e3a3d0b4faf02161738d43
Ralf Handl <ralf.handl@gmail.com>
2025-07-14 13:31:32 +0200

    Merge branch 'pr/4762' into v3.2-dev

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vitest/config'
+import { jsonSchemaCoveragePlugin } from "@hyperjump/json-schema-coverage/vitest"
 
 export default defineConfig({
+  plugins: [jsonSchemaCoveragePlugin()],
   test: {
+    globalSetup: ["tests/schema/oas-schema.mjs"],
+    coverage: {
+      include: ["src/schemas/validation/**/*.yaml"],
+      thresholds: process.env.BASE !== "dev" ? {
+        100: true
+      } : {}
+    },
     forceRerunTriggers: ['**/scripts/**', '**/tests/**'],
     testTimeout: 10000, // 10 seconds
   },
